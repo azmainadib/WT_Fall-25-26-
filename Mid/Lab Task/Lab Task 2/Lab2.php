baa7943071fb89a1e00c2074f3cfbb77353d805a
Azmain Adib <azmain7461@gmail.com>
2025-11-17 11:38:08 +0600

    Updated project files

--- Mid/Lab Task/Lab Task 2/Lab2.php
+++ Mid/Lab Task/Lab Task 2/Lab2.php
@@ -1,1 +1,24 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Event Registration<title>
+ <style>
+    body {
+        font-family:Arial;
+        margin:40px;
+        background:lightcoral;
+    }
+
+    h1,h2{
+        color: black;
+    }
+
+    .container{
+        background: white;
+        padding: 20px:
+        border-radius: 8px;
+        border-radius: 8px;
+        width: auto;
+
+    }
